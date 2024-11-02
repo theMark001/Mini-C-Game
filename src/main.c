b15ce7c8c6c0241f1fd594342cbab7e36f67e000
GNUS <60074926+theMark001@users.noreply.github.com>
2024-11-02 03:37:33 +0100

    several imgs

--- src/main.c
+++ src/main.c
@@ -1,19 +1,23 @@
 #include <mlx.h>
 
-int main(void)
-{
-  // Setting up mlx
+int main(void) {
+  // Setting up MLX
   void *mlx = mlx_init();
 
-  // Setting up window
+  // Setting up Window
   void *win = mlx_new_window(mlx, 500, 500, "Mini Game");
 
-  // Setting up img
-  int img_width;
-  int img_height;
-  void *img = mlx_xpm_file_to_image(mlx, "textures/photo.xpm", &img_width, &img_height);
-  mlx_put_image_to_window(mlx, win, img, 0, 0);
+  // Setting up Imgs
+  int player_width, player_height;
+  void *player_img = mlx_xpm_file_to_image(mlx, "textures/player.xpm", &player_width, &player_height);
 
-  // Loop for project
+  int enemy_width, enemy_height;
+  void *enemy_img = mlx_xpm_file_to_image(mlx, "textures/item.xpm", &enemy_width, &enemy_height);
+
+  // Display the images in the window
+  mlx_put_image_to_window(mlx, win, player_img, 50, 50);   // Display player image at (50, 50)
+  mlx_put_image_to_window(mlx, win, enemy_img, 300, 300);  // Display enemy image at (300, 300)
+
+  // Keep the window open
   mlx_loop(mlx);
 }
