98e3e501e5dfd6950eb7778284a65c3616563874
GNUS <60074926+theMark001@users.noreply.github.com>
2024-11-02 03:28:24 +0100

    good img

--- src/main.c
+++ src/main.c
@@ -3,20 +3,15 @@
 int main(void)
 {
   // Setting up mlx
-  void *mlx;
-  mlx = mlx_init();
+  void *mlx = mlx_init();
 
   // Setting up window
-  void *win;
-  win = mlx_new_window(mlx, 500, 500, "Image Display");
+  void *win = mlx_new_window(mlx, 500, 500, "Mini Game");
 
-
-  void *img;
-  char *relative_path = "textures/photo.xpm";
+  // Setting up img
   int img_width;
   int img_height;
-
-  img = mlx_xpm_file_to_image(mlx, relative_path, &img_width, &img_height);
+  void *img = mlx_xpm_file_to_image(mlx, "textures/photo.xpm", &img_width, &img_height);
   mlx_put_image_to_window(mlx, win, img, 0, 0);
 
   // Loop for project
