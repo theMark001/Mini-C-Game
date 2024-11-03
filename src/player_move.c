3b48769b8b9759475f7d4c89d28824a3cd35aad2
GNUS <60074926+theMark001@users.noreply.github.com>
2024-11-03 21:48:21 +0100

    norminette mini_game.c map_info.c half-of-map_check.c mini_game.h put_img.c half-of-player_move.c   --working

--- src/player_move.c
+++ src/player_move.c
@@ -6,7 +6,7 @@
 /*   By: marksylaiev <marksylaiev@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 23:21:05 by marksylaiev       #+#    #+#             */
-/*   Updated: 2024/11/03 21:43:22 by marksylaiev      ###   ########.fr       */
+/*   Updated: 2024/11/03 21:47:47 by marksylaiev      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,53 +53,50 @@ int	close_window(t_vars *vars)
 	exit(0);
 	return (0);
 }
-
-int	move_player(int key, t_vars *vars)
+void	update_position(int key, int *next_x, int *next_y, t_vars *vars)
 {
-	int		next_x;
-	int		next_y;
-	char	tile;
-	int		already_saved;
-	int		i;
-
-	next_x = vars->state.current_x;
-	next_y = vars->state.current_y;
 	if (key == KEY_W)
-		next_y -= vars->state.tile_size;
+		*next_y -= vars->state.tile_size;
 	else if (key == KEY_A)
-		next_x -= vars->state.tile_size;
+		*next_x -= vars->state.tile_size;
 	else if (key == KEY_S)
-		next_y += vars->state.tile_size;
+		*next_y += vars->state.tile_size;
 	else if (key == KEY_D)
-		next_x += vars->state.tile_size;
+		*next_x += vars->state.tile_size;
 	else if (key == KEY_ESC)
 		close_window(vars);
-	else
-		return (0);
-	tile = next_px(next_x, next_y, vars);
+}
+
+int	check_collectible(int next_x, int next_y, char tile, t_vars *vars)
+{
+	int already_saved = 0;
+	int i = 0;
+
+	while (i < vars->state.collectible_count)
+	{
+		if (vars->state.collectible_x[i] == next_x
+			&& vars->state.collectible_y[i] == next_y)
+		{
+			already_saved = 1;
+			break;
+		}
+		i++;
+	}
+	if (!already_saved && vars->state.collectible_count < MAX_COLLECTIBLES)
+	{
+		vars->state.collectible_x[vars->state.collectible_count] = next_x;
+		vars->state.collectible_y[vars->state.collectible_count] = next_y;
+		vars->state.collectible_count++;
+	}
+	return (tile == 'C');
+}
+
+int	handle_tile_action(char tile, int next_x, int next_y, t_vars *vars)
+{
 	if (tile == '1')
 		return (0);
 	else if (tile == 'C')
-	{
-		already_saved = 0;
-		i = 0;
-		while (i < vars->state.collectible_count)
-		{
-			if (vars->state.collectible_x[i] == next_x
-				&& vars->state.collectible_y[i] == next_y)
-			{
-				already_saved = 1;
-				break ;
-			}
-			i++;
-		}
-		if (!already_saved && vars->state.collectible_count < MAX_COLLECTIBLES)
-		{
-			vars->state.collectible_x[vars->state.collectible_count] = next_x;
-			vars->state.collectible_y[vars->state.collectible_count] = next_y;
-			vars->state.collectible_count++;
-		}
-	}
+		check_collectible(next_x, next_y, tile, vars);
 	else if (tile == 'E')
 	{
 		if (vars->map_info.all_collectible == vars->state.collectible_count)
@@ -107,13 +104,15 @@ int	move_player(int key, t_vars *vars)
 		else
 			return (0);
 	}
-	vars->state.current_x = next_x;
-	vars->state.current_y = next_y;
-	vars->state.move_count++;
-	printf("Move number: %d \n", vars->state.move_count);
+	return (1);
+}
+
+void	refresh_display(t_vars *vars)
+{
+	int i = 0;
+
 	mlx_clear_window(vars->graphics.mlx, vars->graphics.win);
 	read_and_display_map(vars);
-	i = 0;
 	while (i < vars->state.collectible_count)
 	{
 		mlx_put_image_to_window(vars->graphics.mlx, vars->graphics.win,
@@ -127,5 +126,26 @@ int	move_player(int key, t_vars *vars)
 	mlx_put_image_to_window(vars->graphics.mlx, vars->graphics.win,
 		vars->graphics.player_img, vars->state.current_x,
 		vars->state.current_y);
+}
+
+int	move_player(int key, t_vars *vars)
+{
+	int next_x = vars->state.current_x;
+	int next_y = vars->state.current_y;
+
+	update_position(key, &next_x, &next_y, vars);
+	if (key == KEY_ESC)
+		return (0);
+
+	char tile = next_px(next_x, next_y, vars);
+	if (!handle_tile_action(tile, next_x, next_y, vars))
+		return (0);
+
+	vars->state.current_x = next_x;
+	vars->state.current_y = next_y;
+	vars->state.move_count++;
+	printf("Move number: %d \n", vars->state.move_count);
+
+	refresh_display(vars);
 	return (0);
 }
