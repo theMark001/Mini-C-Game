4a9107ea4ab22bfd97a4faf53e20f41811d2bffa
GNUS <60074926+theMark001@users.noreply.github.com>
2024-11-03 19:50:20 +0100

    norminette for put_img and minig_game map_info

--- src/map_info.c
+++ src/map_info.c
@@ -6,61 +6,56 @@
 /*   By: marksylaiev <marksylaiev@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 23:21:05 by marksylaiev       #+#    #+#             */
-/*   Updated: 2024/11/03 03:46:15 by marksylaiev      ###   ########.fr       */
+/*   Updated: 2024/11/03 19:48:50 by marksylaiev      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini_game.h"
 
-void get_map_info(const char *filename, t_vars *vars)
+void	update_map_info(char c, t_vars *vars, int *is_first_line)
 {
-    int fd = open(filename, O_RDONLY);
-    if (fd == -1)  // Check if file opened successfully
-    {
-        printf("Map file don't exist \n");
-        exit(0);
-    }
+	if (c == '\n')
+	{
+		vars->map_info.height++;
+		*is_first_line = 0;
+	}
+	else
+	{
+		if (*is_first_line)
+			vars->map_info.width++;
+		if (c == 'C')
+			vars->map_info.all_collectible++;
+		else if (c == 'E')
+			vars->map_info.all_exits++;
+		else if (c == 'P')
+			vars->map_info.all_players++;
+	}
+}
 
-    // Initialize fields to zero to avoid garbage values
-    vars->map_info.width = 0;
-    vars->map_info.height = 0;
-    vars->map_info.all_collectible = 0;
-    vars->map_info.all_exits = 0;
-    vars->map_info.all_players = 0;
+void	get_map_info(const char *filename, t_vars *vars)
+{
+	int		fd;
+	ssize_t	bytes_read;
+	char	buffer[1024];
+	int		is_first_line;
 
-    ssize_t bytes_read;
-    char buffer[1024];
-    int is_first_line = 1;
-
-    // Read and parse file
-    while ((bytes_read = read(fd, buffer, sizeof(buffer))) > 0)
-    {
-        for (ssize_t i = 0; i < bytes_read; i++)
-        {
-            if (buffer[i] == '\n')
-            {
-                vars->map_info.height++;
-                is_first_line = 0;
-            }
-            else
-            {
-                if (is_first_line)
-                    vars->map_info.width++;
-                if (buffer[i] == 'C')
-                    vars->map_info.all_collectible++;
-                if (buffer[i] == 'E')
-                    vars->map_info.all_exits++;
-                if (buffer[i] == 'P')
-                    vars->map_info.all_players++;
-            }
-        }
-    }
-    
-    // Account for the last line if it doesn't end with a newline
-    vars->map_info.height += 1;
-
-    // Debug output to verify values
-    printf("Height: %d, Width: %d, All collectible: %d, All exits: %d, All players: %d \n", vars->map_info.height, vars->map_info.width, vars->map_info.all_collectible, vars->map_info.all_exits, vars->map_info.all_players);
-
-    close(fd);
+	is_first_line = 1;
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		printf("Map file doesn't exist\n");
+		exit(0);
+	}
+	vars->map_info.width = 0;
+	vars->map_info.height = 0;
+	vars->map_info.all_collectible = 0;
+	vars->map_info.all_exits = 0;
+	vars->map_info.all_players = 0;
+	while ((bytes_read = read(fd, buffer, sizeof(buffer))) > 0)
+	{
+		for (ssize_t i = 0; i < bytes_read; i++)
+			update_map_info(buffer[i], vars, &is_first_line);
+	}
+	vars->map_info.height += 1;
+	close(fd);
 }
