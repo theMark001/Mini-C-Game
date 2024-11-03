45cf5c48b618fdc71b7b715d5d82a9c42194d034
GNUS <60074926+theMark001@users.noreply.github.com>
2024-11-03 21:15:12 +0100

    norminette mini_game.c map_info.c

--- src/map_info.c
+++ src/map_info.c
@@ -6,7 +6,7 @@
 /*   By: marksylaiev <marksylaiev@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 23:21:05 by marksylaiev       #+#    #+#             */
-/*   Updated: 2024/11/03 20:39:37 by marksylaiev      ###   ########.fr       */
+/*   Updated: 2024/11/03 21:14:48 by marksylaiev      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,30 +32,48 @@ void	update_map_info(char c, t_vars *vars, int *is_first_line)
 	}
 }
 
+void	initialize_map_info(t_vars *vars, int *is_first_line)
+{
+	vars->map_info.width = 0;
+	vars->map_info.height = 0;
+	vars->map_info.all_collectible = 0;
+	vars->map_info.all_exits = 0;
+	vars->map_info.all_players = 0;
+	*is_first_line = 1;
+}
+
+void	read_map_data(int fd, t_vars *vars, int *is_first_line)
+{
+	ssize_t	bytes_read;
+	ssize_t	i;
+	char	buffer[1024];
+
+	bytes_read = read(fd, buffer, sizeof(buffer));
+	while (bytes_read > 0)
+	{
+		i = 0;
+		while (i < bytes_read)
+		{
+			update_map_info(buffer[i], vars, is_first_line);
+			i++;
+		}
+		bytes_read = read(fd, buffer, sizeof(buffer));
+	}
+}
+
 void	get_map_info(t_vars *vars)
 {
-	int		fd;
-	ssize_t	bytes_read;
-	char	buffer[1024];
-	int		is_first_line;
+	int	fd;
+	int	is_first_line;
 
-	is_first_line = 1;
 	fd = open(vars->path, O_RDONLY);
 	if (fd == -1)
 	{
 		printf("Map file doesn't exist\n");
 		exit(0);
 	}
-	vars->map_info.width = 0;
-	vars->map_info.height = 0;
-	vars->map_info.all_collectible = 0;
-	vars->map_info.all_exits = 0;
-	vars->map_info.all_players = 0;
-	while ((bytes_read = read(fd, buffer, sizeof(buffer))) > 0)
-	{
-		for (ssize_t i = 0; i < bytes_read; i++)
-			update_map_info(buffer[i], vars, &is_first_line);
-	}
+	initialize_map_info(vars, &is_first_line);
+	read_map_data(fd, vars, &is_first_line);
 	vars->map_info.height += 1;
 	close(fd);
 }
