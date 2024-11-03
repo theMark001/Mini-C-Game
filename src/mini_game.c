3ce202fec78ca525e05b95bad356017a723dd453
GNUS <60074926+theMark001@users.noreply.github.com>
2024-11-03 19:44:55 +0100

    norminette for put_img and minig_game.c

--- src/mini_game.c
+++ src/mini_game.c
@@ -6,51 +6,50 @@
 /*   By: marksylaiev <marksylaiev@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 23:21:05 by marksylaiev       #+#    #+#             */
-/*   Updated: 2024/11/03 03:41:07 by marksylaiev      ###   ########.fr       */
+/*   Updated: 2024/11/03 19:35:36 by marksylaiev      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini_game.h"
 
+void	set_img(t_vars *vars)
+{
+	int	img_width;
+	int	img_height;
+
+	vars->graphics.bg_img = mlx_xpm_file_to_image(vars->graphics.mlx,
+			"textures/bg.xpm", &img_width, &img_height);
+	vars->graphics.player_img = mlx_xpm_file_to_image(vars->graphics.mlx,
+			"textures/player.xpm", &img_width, &img_height);
+	vars->graphics.exit_img = mlx_xpm_file_to_image(vars->graphics.mlx,
+			"textures/exit.xpm", &img_width, &img_height);
+	vars->graphics.item_img = mlx_xpm_file_to_image(vars->graphics.mlx,
+			"textures/item.xpm", &img_width, &img_height);
+	vars->graphics.wall_img = mlx_xpm_file_to_image(vars->graphics.mlx,
+			"textures/wall.xpm", &img_width, &img_height);
+}
+
 int	main(int ac, char **av)
 {
+	t_vars	vars;
+	int		win_width;
+	int		win_height;
+
 	if (ac != 2)
 		return (0);
-		
-	t_vars		vars;
-	int			win_width;
-	int			win_height;
-
-	// Setting up MLX
 	vars.graphics.mlx = mlx_init();
 	vars.state.initial_position_set = 0;
-	// Setting up Window
 	get_map_info(av[1], &vars);
-	if (map_check(av[1], &vars) == 1) {
-		exit (0);
-	}
+	if (map_check(av[1], &vars) == 1)
+		exit(0);
 	vars.state.tile_size = 50;
 	win_width = vars.map_info.width * vars.state.tile_size;
 	win_height = vars.map_info.height * vars.state.tile_size;
 	vars.graphics.win = mlx_new_window(vars.graphics.mlx, win_width, win_height,
 			"Mini Game");
-	// Setting up Imgs
-	int img_width, img_height;
-	vars.graphics.bg_img = mlx_xpm_file_to_image(vars.graphics.mlx,
-			"textures/bg.xpm", &img_width, &img_height);
-	vars.graphics.player_img = mlx_xpm_file_to_image(vars.graphics.mlx,
-			"textures/player.xpm", &img_width, &img_height);
-	vars.graphics.exit_img = mlx_xpm_file_to_image(vars.graphics.mlx,
-			"textures/exit.xpm", &img_width, &img_height);
-	vars.graphics.item_img = mlx_xpm_file_to_image(vars.graphics.mlx,
-			"textures/item.xpm", &img_width, &img_height);
-	vars.graphics.wall_img = mlx_xpm_file_to_image(vars.graphics.mlx,
-			"textures/wall.xpm", &img_width, &img_height);
-	// Display Imgs
+	set_img(&vars);
 	read_and_display_map(&vars, "maps/map.ber");
-	// Setting up Movements
 	mlx_hook(vars.graphics.win, 2, 1L << 0, move_player, &vars);
-	// Set the close event hook to listen for the X button
 	mlx_hook(vars.graphics.win, 17, 0, close_window, &vars);
 	mlx_loop(vars.graphics.mlx);
 	return (0);
--- src/put_img.c
+++ src/put_img.c
@@ -6,7 +6,7 @@
 /*   By: marksylaiev <marksylaiev@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 23:21:05 by marksylaiev       #+#    #+#             */
-/*   Updated: 2024/11/02 23:50:28 by marksylaiev      ###   ########.fr       */
+/*   Updated: 2024/11/03 19:44:18 by marksylaiev      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,29 @@ void	put_img(void *img, int x, int y, t_vars *vars)
 		* vars->state.tile_size, y * vars->state.tile_size);
 }
 
+void	put_img_by_type(char type, int x, int y, t_vars *vars)
+{
+	if (type == '0')
+		put_img(vars->graphics.bg_img, x, y, vars);
+	else if (type == '1')
+		put_img(vars->graphics.wall_img, x, y, vars);
+	else if (type == 'C')
+		put_img(vars->graphics.item_img, x, y, vars);
+	else if (type == 'E')
+		put_img(vars->graphics.exit_img, x, y, vars);
+	else if (type == 'P')
+	{
+		if (!vars->state.initial_position_set)
+		{
+			vars->state.current_x = x * vars->state.tile_size;
+			vars->state.current_y = y * vars->state.tile_size;
+			vars->state.initial_position_set = 1;
+			init_start_pos(vars);
+		}
+		put_img(vars->graphics.player_img, x, y, vars);
+	}
+}
+
 void	read_and_display_map(t_vars *vars, const char *map_path)
 {
 	int		fd;
@@ -31,47 +54,21 @@ void	read_and_display_map(t_vars *vars, const char *map_path)
 	int		x;
 	int		y;
 
-	fd = open(map_path, O_RDONLY);
 	x = 0;
 	y = 0;
+	fd = open(map_path, O_RDONLY);
 	while (read(fd, &buffer, 1) > 0)
 	{
-		if (buffer == '0')
-		{
-			put_img(vars->graphics.bg_img, x, y, vars);
-			x++;
-		}
-		else if (buffer == '1')
-		{
-			put_img(vars->graphics.wall_img, x, y, vars);
-			x++;
-		}
-		else if (buffer == 'C')
-		{
-			put_img(vars->graphics.item_img, x, y, vars);
-			x++;
-		}
-		else if (buffer == 'E')
-		{
-			put_img(vars->graphics.exit_img, x, y, vars);
-			x++;
-		}
-		else if (buffer == 'P')
-		{
-			if (!vars->state.initial_position_set)
-			{
-				vars->state.current_x = x * vars->state.tile_size;
-				vars->state.current_y = y * vars->state.tile_size;
-				vars->state.initial_position_set = 1;
-				init_start_pos(vars);
-			}
-			put_img(vars->graphics.player_img, x, y, vars);
-			x++;
-		}
-		else if (buffer == '\n')
+		if (buffer == '\n')
 		{
 			y++;
 			x = 0;
 		}
+		else
+		{
+			put_img_by_type(buffer, x, y, vars);
+			x++;
+		}
 	}
+	close(fd);
 }
