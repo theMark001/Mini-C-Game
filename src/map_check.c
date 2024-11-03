3258fda81236a45a326b0071bfd96a0b3d11f48f
GNUS <60074926+theMark001@users.noreply.github.com>
2024-11-03 21:33:49 +0100

    norminette mini_game.c map_info.c half-of-map_check.c

--- src/map_check.c
+++ src/map_check.c
@@ -5,116 +5,151 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: marksylaiev <marksylaiev@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/02 23:21:05 by marksylaiev       #+#    #+#             */
-/*   Updated: 2024/11/03 21:07:58 by marksylaiev      ###   ########.fr       */
+/*   Created: 2024/11/03 21:30:23 by marksylaiev       #+#    #+#             */
+/*   Updated: 2024/11/03 21:30:23 by marksylaiev      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini_game.h"
 
-int is_rectangular(const char *path)
+int	check_and_update_length(int *current_length, int *first_length)
 {
-    int fd = open(path, O_RDONLY);
-    ssize_t bytes_read;
-    char buffer[1024];
-    int first_line_length = -1;
-    int current_line_length = 0;
-
-    // Read the file character by character
-    while ((bytes_read = read(fd, buffer, sizeof(buffer))) > 0) {
-        for (ssize_t i = 0; i < bytes_read; i++) {
-            if (buffer[i] == '\n') {
-                if (first_line_length == -1) {
-                    first_line_length = current_line_length;
-                } else if (current_line_length != first_line_length) {
-                    close(fd);
-                    return 0;
-                }
-                current_line_length = 0;  // Reset for the next line
-            } else {
-                current_line_length++;
-            }
-        }
-    }
-
-    // Handle the last line if it doesn't end with a newline
-    if (current_line_length != 0 && current_line_length != first_line_length) {
-        close(fd);
-        return 0;
-    }
-
-    close(fd);
-    return 1;
+	if (*first_length == -1)
+		*first_length = *current_length;
+	else if (*current_length != *first_length)
+		return (0);
+	*current_length = 0;
+	return (1);
 }
 
-int is_enclosed_in_walls(const char *path)
+int	read_and_check_line(int *current_length, int *first_length, char c)
 {
-    int fd = open(path, O_RDONLY);
-    ssize_t bytes_read;
-    char buffer[1024];
-    int line_length = -1;
-    int is_first_line = 1;
-    int is_last_line = 0;
-
-    while ((bytes_read = read(fd, buffer, sizeof(buffer) - 1)) > 0) {
-        buffer[bytes_read] = '\0';
-        char *line_start = buffer;
-
-        for (ssize_t i = 0; i < bytes_read; i++) {
-            if (buffer[i] == '\n' || buffer[i] == '\0') {
-                int current_line_length = &buffer[i] - line_start;
-
-                // Set the line length based on the first line
-                if (line_length == -1) {
-                    line_length = current_line_length;
-                }
-
-                // Check the first and last line entirely for '1's
-                if (is_first_line || is_last_line) {
-                    for (int j = 0; j < current_line_length; j++) {
-                        if (line_start[j] != '1') {
-                            close(fd);
-                            return 0;
-                        }
-                    }
-                } else {  // For middle lines, only check first and last characters
-                    if (line_start[0] != '1' || line_start[current_line_length - 1] != '1') {
-                        close(fd);
-                        return 0;
-                    }
-                }
-
-                // Set up for the next line
-                line_start = &buffer[i + 1];
-                is_first_line = 0;
-                is_last_line = (buffer[i + 1] == '\0');
-            }
-        }
-    }
-
-    close(fd);
-    return 1; // Return 1 for success if all checks pass
+	if (c == '\n')
+		return (check_and_update_length(current_length, first_length));
+	(*current_length)++;
+	return (1);
 }
 
-int map_check(t_vars *vars)
+int	is_rectangular(const char *path)
 {
-    if (vars->map_info.all_players != 1) {
-        printf("Error: map should contain exactly one player.\n");
-        return 1;
-    } else if (vars->map_info.all_exits != 1) {
-        printf("Error: map should contain exactly one exit.\n");
-        return 1;
-    } else if (vars->map_info.all_collectible < 1) {
-        printf("Error: map should contain at least one collectible.\n");
-        return 1;
-    }
- 		else if (is_rectangular(vars->path) != 1) {
-			printf("Error: map is not rectangular.\n");
-			return 1;
+	int		fd;
+	ssize_t	bytes_read;
+	char	buffer[1024];
+	int		first_length;
+	int		current_length;
+	ssize_t	i;
+
+	first_length = -1;
+	current_length = 0;
+	i = 0;
+	fd = open(path, O_RDONLY);
+	while ((bytes_read = read(fd, buffer, sizeof(buffer))) > 0)
+	{
+		i = 0;
+		while (i < bytes_read)
+		{
+			if (!read_and_check_line(&current_length, &first_length, buffer[i]))
+				return (close(fd), 0);
+			i++;
 		}
-		else if (!is_enclosed_in_walls(vars->path)) {
-				printf("Error: map is not enclosed in walls.\n");
-				return 1;
+	}
+	close(fd);
+	return (first_length == -1 || current_length == first_length);
+}
+
+int	process_wall_line(const char *line, int length)
+{
+	int	i;
+
+	i = 0;
+	while (i < length)
+	{
+		if (line[i] != '1')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	process_side_walls(const char *line, int length)
+{
+	return (line[0] == '1' && line[length - 1] == '1');
+}
+
+int	is_enclosed_in_walls(const char *path)
+{
+	int		fd;
+	ssize_t	bytes_read;
+	char	buffer[1024];
+	char	*line_start;
+	int		line_length;
+	int		current_length;
+	int		is_first;
+	ssize_t	i;
+
+	line_length = -1;
+	is_first = 1;
+	fd = open(path, O_RDONLY);
+	while ((bytes_read = read(fd, buffer, sizeof(buffer) - 1)) > 0)
+	{
+		buffer[bytes_read] = '\0';
+		line_start = buffer;
+		i = 0;
+		while (i < bytes_read)
+		{
+			if (buffer[i] == '\n' || buffer[i] == '\0')
+			{
+				current_length = &buffer[i] - line_start;
+				if (line_length == -1)
+					line_length = current_length;
+				if ((is_first || buffer[i + 1] == '\0')
+					&& !process_wall_line(line_start, current_length))
+					return (close(fd), 0);
+				else if (!is_first && !process_side_walls(line_start,
+						current_length))
+					return (close(fd), 0);
+				line_start = &buffer[i + 1];
+				is_first = 0;
+			}
+			i++;
 		}
-		return 0;
+	}
+	return (close(fd), 1);
+}
+
+int	map_check_conditions(t_vars *vars)
+{
+	if (vars->map_info.all_players != 1)
+	{
+		printf("Error: map should contain exactly one player.\n");
+		return (1);
+	}
+	if (vars->map_info.all_exits != 1)
+	{
+		printf("Error: map should contain exactly one exit.\n");
+		return (1);
+	}
+	if (vars->map_info.all_collectible < 1)
+	{
+		printf("Error: map should contain at least one collectible.\n");
+		return (1);
+	}
+	return (0);
+}
+
+int	map_check(t_vars *vars)
+{
+	if (map_check_conditions(vars))
+		return (1);
+	if (!is_rectangular(vars->path))
+	{
+		printf("Error: map is not rectangular.\n");
+		return (1);
+	}
+	if (!is_enclosed_in_walls(vars->path))
+	{
+		printf("Error: map is not enclosed in walls.\n");
+		return (1);
+	}
+	return (0);
 }
